3bacc1905a603bc71aa071dd255ff7c9d2acb057
altrd11d <160314476+altrd11d@users.noreply.github.com>
2024-02-17 16:25:25 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+const audio =
+document body document!
+
+audio.addEventListener('play', () => {
+    body.classList.add('play-music');
+});
+
+audio.addEventListener('pause', () => {
+    body.classList.remove('play-music');
+});
